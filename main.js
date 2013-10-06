b9e2bb0a53f5468b1eba8efbe522c05448deef26
Calvin Park <nofxincubus@gmail.com>
2013-10-06 19:35:38 -0400

    leaflet added to main.js for main functionality

--- main.js
+++ main.js
@@ -19,21 +19,51 @@ function el(s)
 
 function initialize(){
 	
-
-	// Init Canvas
-	var c=document.getElementById("myCanvas");
-	var ctx = c.getContext("2d");
-	// Create Circle
-	ctx.beginPath();
-	ctx.arc(300,300,50,0,2*Math.PI);
-	ctx.stroke();
-	
+/*
 	var mousewheelevt=(/Firefox/i.test(navigator.userAgent))? "DOMMouseScroll" : "mousewheel" //FF doesn't recognize mousewheel as of FF3.x
 	 
 	if (document.attachEvent) //if IE (and Opera depending on user setting)
 		document.attachEvent("on"+mousewheelevt, createUAV(ctx, 100,100))
 	else if (document.addEventListener) //WC3 browsers
 		document.addEventListener(mousewheelevt, createUAV(ctx, 100,100), false)
+
+*/
+	var map = L.map('map').setView([51.505, -0.09], 13);
+
+		L.tileLayer('http://{s}.tile.cloudmade.com/9e8f098a2ebf41158e0423a8b807ec95/997/256/{z}/{x}/{y}.png', {
+			maxZoom: 18,
+			attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>'
+		}).addTo(map);
+
+
+		L.marker([51.5, -0.09]).addTo(map)
+			.bindPopup("<b>Hello world!</b><br />I am a popup.").openPopup();
+
+		L.circle([51.508, -0.11], 500, {
+			color: 'red',
+			fillColor: '#f03',
+			fillOpacity: 0.5
+		}).addTo(map).bindPopup("I am a circle.");
+
+		L.polygon([
+			[51.509, -0.08],
+			[51.503, -0.06],
+			[51.51, -0.047]
+		]).addTo(map).bindPopup("I am a polygon.");
+
+		
+
+
+		var popup = L.popup();
+
+		function onMapClick(e) {
+			popup
+				.setLatLng(e.latlng)
+				.setContent("You clicked the map at " + e.latlng.toString())
+				.openOn(map);
+		}
+
+		map.on('click', onMapClick);
 }
 		
 	
