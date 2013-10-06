a1fe7ef674049e37c36db0fbf4bc3d61a6956a0f
Calvin Park <nofxincubus@gmail.com>
2013-10-06 18:07:07 -0400

    initial leaflet

---
+++ uav_unit.js
@@ -0,0 +1,21 @@
+// JavaScript Document
+
+
+function UAV(){
+	//Basic Info
+	this.latitude = 0;
+	this.longitude = 0;
+	this.altitude = 0;
+
+	//0 to 360
+	this.heading = 0;
+
+	//Velocity
+	this.Velocity = 0;
+	this.vAlt = 0;
+
+	//Fuel
+	this.fuelConsumption = 0;
+	this.fuelLevel = 0;
+
+}
